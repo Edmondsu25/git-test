74ffa73d34cd5187e10f0603718a4b52c4895e55
edmondsu25 <edmondsu@naver.com>
2025-09-24 19:40:58 +0900

    첫 과제

---
+++ assignment.c
@@ -0,0 +1,17 @@
+# define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	printf("두 수를 입력하시오");
+	scanf_s("%d%d", &a, &b);
+	printf("%d+%d=%d\n", a, b, a + b);
+	printf("%d-%d=%d\n", a, b, a - b);
+	printf("%d*%d=%d\n", a, b, a * b);
+	printf("%d/%d=%d\n", a, b, a / b);
+	printf("%d%%%d=%d\n", a, b, a % b);
+
+	return 0;
+}
